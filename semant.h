6e665da5bea15efe58325f86206c2c1824911767
rechardchen <rechardchen@gmail.com>
2017-05-04 00:06:12 +0800

    update

--- semant.h
+++ semant.h
@@ -1,1 +1,13 @@
 #pragma once
+#include <utility>
+
+namespace tiger {
+	struct TrExp;
+	struct Type;
+
+	class Semant {
+	public:
+		typedef std::pair<TrExp*, Type*> ExpTy;
+
+	};
+}
--- translate.h
+++ translate.h
@@ -1,1 +1,37 @@
 #pragma once
+#include "temp.h"
+#include <vector>
+
+namespace tiger {
+	struct TExp;
+	struct TStm;
+	struct TCjump;
+
+	struct TrExp {
+		virtual TExp* unEx() = 0;
+		virtual TStm* unNx() = 0;
+		virtual TStm* unCx(Label t, Label f) = 0;
+	};
+
+	struct TrEx : public TrExp {
+		virtual TExp* unEx() override { return e; }
+		virtual TStm* unNx() override;
+		virtual TStm* unCx(Label t, Label f) override;
+
+		TExp* e;
+	};
+
+	struct TrNx : public TrExp {
+		virtual TExp* unEx() override;
+		virtual TStm* unNx() override { return s; }
+		virtual TStm* unCx(Label t, Label f) override;
+		TStm* s;
+	};
+
+	struct TrCx : public TrExp {
+		virtual TExp* unEx() override;
+		virtual TStm* unNx() override;
+		virtual TStm* unCx(Label t, Label f) override;
+		TCjump* cj;
+	};
+}
